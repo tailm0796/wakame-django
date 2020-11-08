ecf4993901ebb7937f7b17af178c9baed3799afb
mctminhtai <mct.minhtai@gmail.com>
2020-11-08 12:50:42 +0700

    them tinh nang

--- wakame_web/urls.py
+++ wakame_web/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('news.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/',include('accounts.urls')),
 ]
