ef1bd05079d159c8bf9662d87bde9a8107d793e9
mctminhtai <mct.minhtai@gmail.com>
2020-11-10 01:24:39 +0700

    buoc phai dang nhap moi co the sua doi

--- news/views.py
+++ news/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -11,15 +12,22 @@ class HomePageNews(ListView):
 class HomePageNewsDetail(DetailView):
     model = Post
     template_name = 'news_detail.html'
-class HomePageNewsCreate(CreateView):
+class HomePageNewsCreate(LoginRequiredMixin,CreateView):
     model = Post
     template_name = 'news_create.html'
-    fields = '__all__'
-class HomePageNewsEdit(UpdateView):
+    fields = ('title','imglink','body','recap')
+    login_url = 'login'
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+class HomePageNewsEdit(LoginRequiredMixin,UpdateView):
     model = Post
     template_name = 'news_edit.html'
-    fields = '__all__'
-class HomePageNewsDelete(DeleteView):
+    #fields = '__all__'
+    fields = ('title','imglink','body','recap')
+    login_url = 'login'
+class HomePageNewsDelete(LoginRequiredMixin,DeleteView):
     model = Post
     template_name = 'news_delete.html'
+    login_url = 'login'
     success_url = reverse_lazy('news')
