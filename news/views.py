fdccda2b2b38532cdf2735c832572a5b1db5bbc7
mctminhtai <mct.minhtai@gmail.com>
2020-11-27 22:55:06 +0700

    them PermissionRequiredMixin

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -13,32 +13,35 @@ class HomePageNews(ListView):
 class HomePageNewsDetail(DetailView):
     model = Post
     template_name = 'news_detail.html'
-class HomePageNewsCreate(LoginRequiredMixin,CreateView):
+class HomePageNewsCreate(LoginRequiredMixin,PermissionRequiredMixin,CreateView):
     model = Post
     template_name = 'news_create.html'
     fields = ('title','body','image')
     login_url = 'login'
+    permission_required = 'news.add_post'
     def form_valid(self,form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-class HomePageNewsEdit(LoginRequiredMixin,UpdateView):
+class HomePageNewsEdit(LoginRequiredMixin,PermissionRequiredMixin,UpdateView):
     model = Post
     template_name = 'news_edit.html'
     #fields = '__all__'
     fields = ('title','body','image')
     login_url = 'login'
+    permission_required = 'news.change_post'
     def dispatch(self, request, *args, **kwargs):
         obj = self.get_object()
         if obj.author != self.request.user:
             raise PermissionDenied
         return super().dispatch(request, *args, **kwargs)
-class HomePageNewsDelete(LoginRequiredMixin,DeleteView):
+class HomePageNewsDelete(LoginRequiredMixin,PermissionRequiredMixin,DeleteView):
     model = Post
     template_name = 'news_delete.html'
     login_url = 'login'
+    permission_required = 'news.delete_post'
     success_url = reverse_lazy('news')
-    def dispatch(self, request, *args, **kwargs):
-        obj = self.get_object()
-        if obj.author != self.request.user:
-            raise PermissionDenied
-        return super().dispatch(request, *args, **kwargs)
+    # def dispatch(self, request, *args, **kwargs):
+    #     obj = self.get_object()
+    #     if obj.author != self.request.user:
+    #         raise PermissionDenied
+    #     return super().dispatch(request, *args, **kwargs)
--- onlstudy/views.py
+++ onlstudy/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -8,34 +8,37 @@ from django.core.exceptions import PermissionDenied
 class LessonsList(ListView):
     model = Lessons
     template_name = 'lessons_list.html'
-class LessonCreate(LoginRequiredMixin,CreateView):
+class LessonCreate(LoginRequiredMixin,PermissionRequiredMixin,CreateView):
     model = Lessons
     template_name = 'lesson_create.html'
     fields = ('title','body','image')
     login_url = 'login'
+    permission_required = 'onlstudy.add_lessons'
     def form_valid(self,form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 class LessonDetail(DetailView):
     model = Lessons
     template_name = 'lesson_detail.html'
-class LessonEdit(LoginRequiredMixin,UpdateView):
+class LessonEdit(LoginRequiredMixin,PermissionRequiredMixin,UpdateView):
     model = Lessons
     template_name = 'lesson_edit.html'
     fields = ('title','body','image')
     login_url = 'login'
+    permission_required = 'onlstudy.change_lessons'
     def dispatch(self, request, *args, **kwargs):
         obj = self.get_object()
         if obj.author != self.request.user:
             raise PermissionDenied
         return super().dispatch(request, *args, **kwargs)
-class LessonDelete(LoginRequiredMixin,DeleteView):
+class LessonDelete(LoginRequiredMixin,PermissionRequiredMixin,DeleteView):
     model = Lessons
     template_name = 'lesson_delete.html'
     login_url = 'login'
+    permission_required = 'onlstudy.delete_lessons'
     success_url = reverse_lazy('lessions_list')
-    def dispatch(self, request, *args, **kwargs):
-        obj = self.get_object()
-        if obj.author != self.request.user:
-            raise PermissionDenied
-        return super().dispatch(request, *args, **kwargs)
+    # def dispatch(self, request, *args, **kwargs):
+    #     obj = self.get_object()
+    #     if obj.author != self.request.user:
+    #         raise PermissionDenied
+    #     return super().dispatch(request, *args, **kwargs)
